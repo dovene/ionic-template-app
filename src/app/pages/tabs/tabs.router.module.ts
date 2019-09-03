f0cd922648abbbf6ede891487fb242313e29150c
Dov <dov@Reykjavik.local>
2019-09-03 14:37:18 +0200

    initial commit

---
+++ src/app/pages/tabs/tabs.router.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'films',
+        children: [
+          {
+            path: '',
+            loadChildren: '../films/films.module#FilmsPageModule'
+          },
+          {
+            path: ':id',
+            loadChildren: '../film-details/film-details.module#FilmDetailsPageModule'
+          }
+        ]
+      },
+      {
+        path: 'people',
+        children: [
+          {
+            path: '',
+            loadChildren: '../people/people.module#PeoplePageModule'
+          }
+        ]
+      },
+      {
+        path: 'planets',
+        children: [
+          {
+            path: '',
+            loadChildren: '../planets/planets.module#PlanetsPageModule'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/films',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [ CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
