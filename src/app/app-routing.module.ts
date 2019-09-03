ddc2af17f60407b46f24a322b1c7596729f08925
Dov <dov@Reykjavik.local>
2019-09-03 17:25:08 +0200

    added a login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-   { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
- ];
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+  //tab module must come last and its path must be null otherwise it does not work 
+  // Because 1 : order matters in path resolution
+  // 2 : we want to preload tab module but not allowing its page to be accessible 
+  // through path access
+  { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' },
+];
 
 @NgModule({
   imports: [
